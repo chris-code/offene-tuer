0e8600461f924e7232d1ee343706a6f6b0ccfb45
chris-code <chris-code@users.noreply.github.com>
2016-03-10 13:25:07 +0100

    Added function to initialize environment

--- src/robotics.py
+++ src/robotics.py
@@ -3,8 +3,19 @@ import numpy
 x = 10.0
 y = 10.0
 theta = 0.0
+speed = 3.0
 
 def do_simulation_step(environment):
 	'''For a given an environment, give the robot's next position and orientation'''
 
 	return x, y, theta
+
+def initialize_environment(height, width):
+	'''Takes width and height and creates a 2D environment of that size'''
+	environment = numpy.zeros(shape=(height, width), dtype=numpy.bool_)
+	environment[:,0] = True
+	environment[:,-1] = True
+	environment[0,:] = True
+	environment[-1,:] = True
+
+	return environment
