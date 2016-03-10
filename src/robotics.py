94e69a135cf24567f522e23cc3ec2152eb208b20
chris-code <chris-code@users.noreply.github.com>
2016-03-10 13:06:51 +0100

    Added environment to simulation parameters

--- src/robotics.py
+++ src/robotics.py
@@ -1,10 +1,10 @@
 import numpy
 
-x = 0.0
-y = 0.0
+x = 10.0
+y = 10.0
 theta = 0.0
 
-def do_simulation_step():
+def do_simulation_step(environment):
 	'''For a given an environment, give the robot's next position and orientation'''
 
 	return x, y, theta
