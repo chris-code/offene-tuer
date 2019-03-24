7bad0f112f6dce0f898ef28ded5035d631fca0a1
chris-code <Christian.Mielers@rub.de>
2019-03-24 18:08:44 +0100

    Made some code PEP8 compliant
    (this was a vim-ale test, nothing to see here)

--- src/main.py
+++ src/main.py
@@ -1,51 +1,64 @@
+"""This module runs the main loop for the robotics simulation."""
+
 import time
 import timeit
 
 import robotics
 import environment
-from terminal_output import Field
+from graphical_output import Field
 
 # Robot parameters
-maximum_speed = 6.0 # Squares per second
-number_of_sensors = 5 # At least 2
-sensor_cone_width = 42
-sensor_mount_angle = 160
-repulsion_force = 35.0
-distance_decay = 1.5
+MAXIMUM_SPEED = 6.0  # Squares per second
+NUMBER_OF_SENSORS = 5  # At least 2
+SENSOR_CONE_WIDTH = 42
+SENSOR_MOUNT_ANGLE = 160
+REPULSION_FORCE = 35.0
+DISTANCE_DECAY = 1.5
 
 # Environment and simulation parameters
-environment_width = 30
-environment_height = 20
-number_of_obstacles = 20
-time_scale = 1 / 120.0
+ENVIRONMENT_WIDTH = 30
+ENVIRONMENT_HEIGHT = 20
+NUMBER_OF_OBSTACLES = 20
+TIME_SCALE = 1 / 120.0
 
-# Initialize environment
-env = environment.Environment(environment_width, environment_height)
-env.add_random_obstacles(number_of_obstacles)
 
-# Initialize robot
-x_start, y_start = env.get_free_position()
-rob = robotics.Robot(x_start, y_start, maximum_speed, number_of_sensors, sensor_cone_width, sensor_mount_angle, repulsion_force, distance_decay, time_scale)
+def main():
+	"""Execute main function."""
+	# Initialize environment
+	env = environment.Environment(ENVIRONMENT_WIDTH, ENVIRONMENT_HEIGHT)
+	env.add_random_obstacles(NUMBER_OF_OBSTACLES)
 
-# Initialize GUI
-field = Field(env)
+	# Initialize robot
+	x_start, y_start = env.get_free_position()
+	rob = robotics.Robot(
+		x_start, y_start, MAXIMUM_SPEED, NUMBER_OF_SENSORS, SENSOR_CONE_WIDTH, SENSOR_MOUNT_ANGLE,
+		REPULSION_FORCE, DISTANCE_DECAY, TIME_SCALE
+	)
 
-# Draw only one out of draw_counter_mod steps
-# This is useful because we compute 1/time_scale simulation steps per second,
-# which is usually a lot more than we want to draw
-fps = 50.0 # 25 is NOT fluent motion
-draw_counter = 0
-draw_counter_mod = max( int(round( (1.0 / time_scale) / fps )), 1)
+	# Initialize GUI
+	field = Field(env)
 
-# Main loop, runs indefinitely
-while(not field.halt):
-	t_start = timeit.default_timer()
-	x, y, theta, speed = rob.step(env)
-	if draw_counter == 0:
-		field.moveBot(x, y, theta)
-		field.paint()
-	draw_counter = (draw_counter + 1) % draw_counter_mod
-	t_end = timeit.default_timer()
+	# Draw only one out of draw_counter_mod steps
+	# This is useful because we compute 1/TIME_SCALE simulation steps per second,
+	# which is usually a lot more than we want to draw
+	fps = 50.0  # 25 is NOT fluent motion
+	draw_counter = 0
+	draw_counter_mod = max(int(round((1.0 / TIME_SCALE) / fps)), 1)
 
-	if time_scale - (t_end - t_start) > 0.0011: # In this case the overhead of time.sleep is not an issue
-		time.sleep( time_scale - (t_end - t_start) )
+	# Main loop, runs indefinitely
+	while not field.halt:
+		t_start = timeit.default_timer()
+		x_pos, y_pos, theta, _ = rob.step(env)
+		if draw_counter == 0:
+			field.moveBot(x_pos, y_pos, theta)
+			field.paint()
+		draw_counter = (draw_counter + 1) % draw_counter_mod
+		t_end = timeit.default_timer()
+
+		# In this case the overhead of time.sleep is not an issue
+		if TIME_SCALE - (t_end - t_start) > 0.0011:
+			time.sleep(TIME_SCALE - (t_end - t_start))
+
+
+if __name__ == "__main__":
+	main()
--- src/robotics.py
+++ src/robotics.py
@@ -1,10 +1,18 @@
+"""This module controls the robot's movements."""
+
 import math
 import random
 
+
 class Robot():
-	def __init__(self, x_start, y_start, maximum_speed, number_of_sensors, sensor_cone_width, sensor_mount_angle, repulsion_force, distance_decay, time_scale):
+	"""This Class implements the algorithms to controll and simulate the robot."""
+
+	def __init__(
+			self, x_start, y_start, maximum_speed, number_of_sensors, sensor_cone_width,
+			sensor_mount_angle, repulsion_force, distance_decay, time_scale):
+		"""Save parameters and initialize the robot's internal state."""
 		self.x, self.y = x_start, y_start
-		self.theta = random.uniform(0, math.pi*2)
+		self.theta = random.uniform(0, math.pi * 2)
 		self.speed = 0
 
 		self.maximum_speed = maximum_speed
@@ -14,7 +22,10 @@ class Robot():
 
 		number_of_sensors = int(round(number_of_sensors))
 		sensor_mount_angle *= (1.0 / 360.0) * 2 * math.pi
-		self.sensor_angles = [sensor_mount_angle * (-0.5 + i * 1.0 / (number_of_sensors-1)) for i in range(number_of_sensors)]
+		self.sensor_angles = [
+			sensor_mount_angle * (-0.5 + i * 1.0 / (number_of_sensors - 1))
+			for i in range(number_of_sensors)
+		]
 		self.sensor_angles.reverse()
 
 		self.stuckness_score = 0
@@ -22,26 +33,27 @@ class Robot():
 		self.time_scale = time_scale
 
 	def step(self, env):
-		'''Given an environment, give the robot's next position and orientation'''
-
+		"""Given an environment, return the robot's next position and orientation."""
 		sensor_data = self.get_sensor_data(env)
 		narrowness_score = self.get_narrowness_score(sensor_data)
-		obstacle_force_x, obstacle_force_y = self.prevent_block_crossing(env) # Physics prevent you from entering obstacles
+
+		# Physics prevents you from entering obstacles
+		obstacle_force_x, obstacle_force_y = self.prevent_block_crossing(env)
 
 		# Calculate movement change
-		x_dot = math.cos(self.theta) * self.maximum_speed * sigmoid(self.speed, beta=4.0) # Move according to current speed and orientation
+		x_dot = math.cos(self.theta) * self.maximum_speed * sigmoid(self.speed, beta=4.0)
 		y_dot = math.sin(self.theta) * self.maximum_speed * sigmoid(self.speed, beta=4.0)
 		x_dot += obstacle_force_x
 		y_dot += obstacle_force_y
 
 		# Calculate orientation change
-		forcelets = [ self.forcelet(sd, sa) for sd, sa in zip(sensor_data, self.sensor_angles) ]
+		forcelets = [self.forcelet(sd, sa) for sd, sa in zip(sensor_data, self.sensor_angles)]
 		stuckness_coeff = sigmoid(self.stuckness_score - 0.78, beta=64)
-		theta_dot = sum(forcelets) * (1-stuckness_coeff)
-		theta_dot += (math.pi/3) * stuckness_coeff
+		theta_dot = sum(forcelets) * (1 - stuckness_coeff)
+		theta_dot += (math.pi / 3) * stuckness_coeff
 
 		# Calculate speed change
-		speed_dot = 3.0 * ( -self.speed - 1.0 + 2.0 * (1-narrowness_score) )
+		speed_dot = 3.0 * (-self.speed - 1.0 + 2.0 * (1 - narrowness_score))
 
 		# Calculate change in stuckness score
 		stuckness_score_dot = -self.stuckness_score + narrowness_score
@@ -56,62 +68,77 @@ class Robot():
 		return self.x, self.y, self.theta, self.speed
 
 	def get_sensor_data(self, env):
+		"""Return the readings from the robot's sensors."""
 		sensor_data = []
-		distances = env.get_distances(self.x, self.y) # Distance to every obstacle
-		angles = env.get_angles(self.x, self.y) # Angle to every obstacle
+		distances = env.get_distances(self.x, self.y)  # Distance to every obstacle
+		angles = env.get_angles(self.x, self.y)  # Angle to every obstacle
 
-		for sa in self.sensor_angles:
-			absolute_sa = self.theta + sa
+		for sensor_angle in self.sensor_angles:
+			absolute_sa = self.theta + sensor_angle
 
 			closest_distance = 2**30
 			for obs_x, obs_y in env:
-				angle_difference = math.atan2( math.sin(absolute_sa - angles[(obs_x, obs_y)]), math.cos(absolute_sa - angles[(obs_x, obs_y)]) )
+				angle_difference = math.atan2(
+					math.sin(absolute_sa - angles[(obs_x, obs_y)]),
+					math.cos(absolute_sa - angles[(obs_x, obs_y)])
+				)
 				if abs(angle_difference) < self.sensor_cone_width / 2.0:
-					closest_distance = min( closest_distance, distances[(obs_x, obs_y)] )
+					closest_distance = min(closest_distance, distances[(obs_x, obs_y)])
 			sensor_data.append(closest_distance)
 
 		return sensor_data
 
 	def forcelet(self, sensor_reading, sensor_angle):
+		"""Compute force induced by sensor reading."""
 		lambd = self.repulsion_force * math.exp(-sensor_reading / self.distance_decay)
-		sigma = math.atan(math.tan(self.sensor_cone_width/2.0) + (1.0 / (1.0 + sensor_reading)))
-		force = lambd * (-sensor_angle) * math.exp(- (sensor_angle)**2/(2*sigma**2))
+		sigma = math.atan(
+			math.tan(self.sensor_cone_width / 2.0)
+			+
+			(1.0 / (1.0 + sensor_reading))
+		)
+		force = lambd * (-sensor_angle) * math.exp(- (sensor_angle)**2 / (2*sigma**2))
 		return force
 
 	def get_narrowness_score(self, sensor_data):
+		"""Return a value indicating how cramped the robot feels."""
 		weighted_distances = [6.0]
-		for sd, sa in zip(sensor_data, self.sensor_angles):
+		for sensor_value, sensor_angle in zip(sensor_data, self.sensor_angles):
 			sigma = 1.25
-			wd = min(sd, 6.0) / math.exp(- sa**2 / (2 * sigma**2))
-			weighted_distances.append(wd)
+			weighted_distance = min(sensor_value, 6.0) / math.exp(- sensor_angle**2 / (2 * sigma**2))
+			weighted_distances.append(weighted_distance)
 
 		return 1 - min(weighted_distances) / 6.0
 
 	def prevent_block_crossing(self, env):
-		'''This function prevents the robot from entering a square occupied by an obstacle.
-		It is a rudimentary way to implement the obstacles as an actual, physical barrier'''
+		"""
+		Prevent the robot from entering a square occupied by an obstacle.
 
+		It is a rudimentary way to implement the obstacles as an actual, physical barrier.
+		"""
 		grid_x, grid_y = int(round(self.x)), int(round(self.y))
-		move_x, move_y = 0, 0 # the movement change induced by obstacles
+		move_x, move_y = 0, 0  # the movement change induced by obstacles
 
 		# Check for collisions with all neighbors
-		direct_neighbors = [(grid_x + offset_x, grid_y + offset_y) for offset_x, offset_y in [(1,0), (0,1), (-1,0), (0,-1)]]
+		direct_neighbors = [(grid_x + offset_x, grid_y + offset_y)
+			for offset_x, offset_y in [(1, 0), (0, 1), (-1, 0), (0, -1)]]
 		for n_x, n_y in direct_neighbors:
 			if (n_x, n_y) in env:
-				if n_x == grid_x and abs(self.y - n_y) < 0.9: # upper / lower neighbor
-					move_y += 1.0 / (self.y - n_y) # move away horizontally
-				elif n_y == grid_y and abs(self.x - n_x) < 0.9: # left / right neighbor
-					move_x += 1.0 / (self.x - n_x) # move away vertically
+				if n_x == grid_x and abs(self.y - n_y) < 0.9:  # upper / lower neighbor
+					move_y += 1.0 / (self.y - n_y)  # move away horizontally
+				elif n_y == grid_y and abs(self.x - n_x) < 0.9:  # left / right neighbor
+					move_x += 1.0 / (self.x - n_x)  # move away vertically
 
 		move_x, move_y = self.maximum_speed * move_x, self.maximum_speed * move_y
 		return move_x, move_y
 
 
-
 # Helper functions
 
-def sigmoid(x, beta=1.0):
-	return 1.0 / (1.0 + math.exp(- beta * x))
+def sigmoid(value, beta=1.0):
+	"""Compute the value of the sigmoid function with slope beta at position value."""
+	return 1.0 / (1.0 + math.exp(- beta * value))
 
-def distance(x1, y1, x2, y2):
-	return math.sqrt((x1-x2)**2 + (y1-y2)**2)
+
+def distance(x_1, y_1, x_2, y_2):
+	"""Compute the distance of two points."""
+	return math.sqrt((x_1 - x_2)**2 + (y_1 - y_2)**2)
